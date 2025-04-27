9b41c5b50af9928430fe919d24fa78a4eee52004
Bunts <dubbzi321@gmail.com>
2025-04-27 20:40:46 +0300

    added requirements + msg history changes

--- message_history.py
+++ message_history.py
@@ -1,7 +1,9 @@
 
+import os
 from google.auth import compute_engine
 from google.cloud import firestore
 from langchain_google_firestore import FirestoreChatMessageHistory
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 PROJECT_ID = "fir-test-ee6a3"
@@ -15,3 +17,23 @@ client = firestore.Client(
 history = FirestoreChatMessageHistory(
     session_id=SESSION_ID, collection=COLLECTION_NAME, client=client
 )
+
+api_key = os.environ["GOOGLE_API_KEY"]
+
+model= ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+while True:
+    prompt = input()
+    history.add_user_message(prompt)
+
+    ai_response = model.invoke(history.messages)
+    history.add_ai_message(ai_response.content)
+
+    print(ai_response)
